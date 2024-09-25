509c3fb08187b7888679f6a9bfa29b399bc301e8
aogonzalez2 <aogonzalez2@csuchico.edu>
2024-09-25 11:16:08 -0700

    Resloved Input Handling, closes #2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-using std::cout, std::cin, std::endl, std::string, std::vector;
+using std::cout, std::cin, std::endl, std::string, std::vector, std::getline;
 
 int main(){
   string input = "";
@@ -14,12 +15,12 @@ int main(){
     else{
       cout << "What is your next favorite?\n";
     }
-    cin >> input;
+    getline(cin, input); 
     list.push_back(input);
   }while( input != "done" );
 
   cout << "Your favorite list:\n";
-  for(int i = 0; i < list.size(); i++){
+  for(int i = 0; i < list.size()-1; i++){
     cout << list.at(i) << endl;
   }
 
